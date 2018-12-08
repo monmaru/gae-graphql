45bee1865b99091ddf900b8f10b73cb1122659c3
Yoshitomo Minami <minami530@gmail.com>
2018-12-08 15:35:41 +0900

    Migrate to GAE go 1.11

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/monmaru/gae-graphql
+
+require (
+	github.com/golang/protobuf v1.2.0
+	github.com/gorilla/context v1.1.1
+	github.com/gorilla/mux v1.6.2
+	github.com/graphql-go/graphql v0.7.7
+	golang.org/x/net v0.0.0-20181207154023-610586996380
+	google.golang.org/appengine v1.3.0
+)
--- main.go
+++ main.go
@@ -1,14 +1,16 @@
-package app
+package main
 
 import (
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/monmaru/gae-graphql/application"
+	"google.golang.org/appengine"
 )
 
-func init() {
+func main() {
 	r := mux.NewRouter()
 	r.Methods("POST").Path("/graphql").HandlerFunc(application.GraphQLHandler)
 	http.Handle("/", r)
+	appengine.Main()
 }
