e694957ab30fb6ffcd0840180fd6da55d6aecf54
Yoshitomo Minami <minami530@gmail.com>
2018-12-09 11:48:54 +0900

    Bye old appengine api

--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/graphql-go/handler v0.2.2
 	go.opencensus.io v0.18.0 // indirect
 	google.golang.org/api v0.0.0-20181206211257-1a5ef82f9af4 // indirect
-	google.golang.org/appengine v1.3.0
 	google.golang.org/genproto v0.0.0-20181202183823-bd91e49a0898 // indirect
 	google.golang.org/grpc v1.17.0 // indirect
 )
--- main.go
+++ main.go
@@ -10,33 +10,37 @@ import (
 	"github.com/monmaru/gae-graphql/application/controller"
 	"github.com/monmaru/gae-graphql/application/gql"
 	"github.com/monmaru/gae-graphql/infrastructure"
-	"google.golang.org/appengine"
 )
 
 func main() {
-	projID := os.Getenv("PROJECT_ID")
-	ud, err := infrastructure.NewUserDatastore(projID)
-	exitIfError(err)
-
-	bd, err := infrastructure.NewBlogDatastore(projID)
-	exitIfError(err)
-
-	schema, _ := gql.NewSchema(ud, bd)
-	h := handler.New(&handler.Config{
-		Schema:   &schema,
-		Pretty:   true,
-		GraphiQL: true,
-	})
-
-	r := mux.NewRouter()
-	r.Path("/api/graphql").Handler(controller.New(schema)).Methods("POST")
-	r.Path("/graphiql").Handler(h)
-	http.Handle("/", r)
-	appengine.Main()
-}
-
-func exitIfError(err error) {
-	if err != nil {
+	if err := run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func run() error {
+	projID := os.Getenv("PROJECT_ID")
+	ud, err := infrastructure.NewUserDatastore(projID)
+	if err != nil {
+		return err
+	}
+
+	bd, err := infrastructure.NewBlogDatastore(projID)
+	if err != nil {
+		return err
+	}
+
+	schema, _ := gql.NewSchema(ud, bd)
+	playground := handler.New(&handler.Config{
+		Schema:     &schema,
+		Pretty:     true,
+		GraphiQL:   false,
+		Playground: true,
+	})
+
+	router := mux.NewRouter()
+	router.Path("/api/graphql").Handler(controller.New(schema)).Methods(http.MethodPost)
+	router.Path("/playground").Handler(playground)
+	http.Handle("/", router)
+	return http.ListenAndServe(":8080", router)
 }
