f22d70af32a7830004a6a4763996d4d85946cb6f
Yoshitomo Minami <minami530@gmail.com>
2018-12-11 22:44:40 +0900

    Added time tracking

--- application/usecase/graphql.go
+++ application/usecase/graphql.go
@@ -2,8 +2,10 @@ package usecase
 
 import (
 	"context"
+	"time"
 
 	"github.com/graphql-go/graphql"
+	"github.com/monmaru/gae-graphql/library/profile"
 )
 
 type GraphQLUsecase interface {
@@ -15,6 +17,7 @@ type GraphQLInteractor struct {
 }
 
 func (i *GraphQLInteractor) Do(ctx context.Context, body string) interface{} {
+	defer profile.Duration(time.Now(), "GraphQLInteractor.Do")
 	return graphql.Do(graphql.Params{
 		Schema:        i.Schema,
 		RequestString: string(body),
